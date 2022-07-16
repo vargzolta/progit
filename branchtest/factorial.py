99af6dfb8b1863f437d87cf9b471cc28e47d5511
Zoltan Varga <vargzolta@outlook.com>
2022-07-16 18:50:22 +0200

    Fix factorial calculator's bug

--- branchtest/factorial.py
+++ branchtest/factorial.py
@@ -1,7 +1,7 @@
 def factorial(num):
-    if num == 1:
+    if num == 0:
         return 1
     else:
-        return factorial(num - 1)
+        return num * factorial(num - 1)
 
 print(factorial(4))
