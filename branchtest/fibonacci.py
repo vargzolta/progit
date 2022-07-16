48081df169c877bc4395aa61fcec126ede52b728
Zoltan Varga <vargzolta@outlook.com>
2022-07-16 18:45:42 +0200

    Add fibonacci calculator on new branch

---
+++ branchtest/fibonacci.py
@@ -0,0 +1,11 @@
+def print_fibonacci(n):
+    previous = 0
+    current = 1
+    for i in range(1, n+1):
+        tmp = previous
+        print(current)
+        previous = current
+        current += tmp
+
+
+print_fibonacci(10)
