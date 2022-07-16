79d751499557267e2590a2daff371db5bd610cf8
Zoltan Varga <vargzolta@outlook.com>
2022-07-16 19:05:32 +0200

    Finish fibonacci program

--- branchtest/fibonacci.py
+++ branchtest/fibonacci.py
@@ -1,7 +1,8 @@
+# prints the first n fibonacci numbers to the console
 def print_fibonacci(n):
     previous = 0
     current = 1
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         tmp = previous
         print(current)
         previous = current
